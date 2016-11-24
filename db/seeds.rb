1acf596014d34a23b4f24c8f935d67361a20863b
Sarah Jammernegg <sjammernegg@gmail.com>
2016-11-24 17:13:31 +0100

    adding ingredient seeds and doses controller.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# TODO: Write a seed to insert 10 posts in the database fetched from the Hacker News API.
+
+puts "Retrieving ingredients"
+response_id = RestClient.get "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+id_array = JSON.parse(response_id)
+
+puts 'Creating ingredients'
+
+# p id_array.first[1]
+id_array.first[1].take(10).each do |element|
+  Ingredient.create!(name: element["strIngredient1"])
+end
+
+puts 'Well done!'
